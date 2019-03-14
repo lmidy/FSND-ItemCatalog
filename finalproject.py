efb23fc094850acecc8f295a46522281de375340
lmidy <46266008+lmidy@users.noreply.github.com>
2019-03-13 22:37:22 -0500

    Add files via upload

--- finalproject.py
+++ finalproject.py
@@ -65,9 +65,8 @@ def gconnect():
         return response
     # Verify if access token is valid/working
     access_token = credentials.access_token
-    url =
-    ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
-     % access_token)
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
     # Handle error if access token is not valid
